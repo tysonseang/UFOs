b006fdb45cf8af4985c4c24dec048cd2b257854d
tysonseang <tyson.seang@gmail.com>
2021-05-13 16:47:16 -0500

    Updated app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ const tableData = data;
 // reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// Create a table function
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -21,3 +22,21 @@ function buildTable(data) {
       );
     });
   }
+
+  function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the rows where the `datetime` value matches 
+      //the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data @NOTE: If no date was entered, 
+     //then filteredData will just be the original tableData.
+    buildTable(filteredData);
+  };
+}
