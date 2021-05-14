7ba60d121a42c47880708899721e61a20e801577
tysonseang <tyson.seang@gmail.com>
2021-05-14 15:35:43 -0500

    Updated app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -35,8 +35,13 @@ function buildTable(data) {
       filteredData = filteredData.filter(row => row.datetime === date);
     };
   
-     // Rebuild the table using the filtered data @NOTE: If no date was entered, 
-     //then filteredData will just be the original tableData.
+     // Rebuild the table using the filtered data 
+     // @NOTE: If no date was entered, then filteredData will just be the original tableData.
     buildTable(filteredData);
-  };
 }
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
